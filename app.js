4a9ba854a011c5e95d4a6226f33c45b9da18adf7
shiba <rp.shiba2@gmail.com>
2022-04-04 02:27:12 +0530

    send token in cookies

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const express = require("express");
 const User = require("./model/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser')
+const  auth  = require("./middleware/database");
 const app = express();
 app.use(express.json());
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
   res.send("hello world");
@@ -53,6 +56,7 @@ app.post("/register", async (req, res) => {
   }
 });
 app.post("/login",async(req,res)=>{
+try{
   const {email,password} = req.body;
   const existingUser= await User.findOne({email});
   if(!existingUser){
@@ -75,10 +79,27 @@ app.post("/login",async(req,res)=>{
   );
   existingUser.token = token;
   //password handle
-  // existingUser.password = undefined;
-  res.status(200).send(existingUser);
-  res.send("login success");
+  existingUser.password = undefined;
+  // res.status(200).send(existingUser);
+  // res.send("login success");
+  return res.cookie("token",token,{
+    httpOnly:true,
+    expires:new Date(Date.now()+1000*60*60*30)
+  }).status(200).json({
+    message:"login success",
+    token,
+    user:existingUser
+  })
 
+}
+catch(err){
+  console.log(err);
+}
+
+});
+
+app.get("/dashboard",auth,(req,res)=>{
+  res.send("Welcome to dashboard");
 })
 
 
